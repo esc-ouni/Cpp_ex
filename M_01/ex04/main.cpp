550fb2c4f9e8e1649e7b8b7b7b7fa096e5fb26bc
esc-ouni <idouni@student.1337.ma>
2023-10-01 16:16:25 +0100

    prgrs

--- M_01/ex04/main.cpp
+++ M_01/ex04/main.cpp
@@ -1,32 +1,50 @@
 #include "main.hpp"
 
+void exit_err(std::string err_msg){
+    std::cerr << err_msg << std::endl;
+    std::exit(1);
+}
+
+void __init(int argc, char *argv[]){
+    std::string holder;
+
+    if (argc == 4){
+        for (int i = 1; i < 4; i++){
+            holder = argv[i];
+            if (holder.empty())
+                exit_err("Err: parse part");
+            holder.clear();
+        }
+    }
+    // exit_err("Err: parse part");
+}
+
+
 int main(int argc, char *argv[])
 {
-    if (argc == 4)
+    __init(argc, argv);
+    std::string line;
+    std::ifstream infile;
+    std::ofstream outfile;
+
+    line.clear();
+    infile.open(argv[1]);
+    if (!infile.is_open())
     {
-        std::string line;
-        std::ifstream infile;
-        std::ofstream outfile;
-
-        line.clear();
-        infile.open(argv[1]);
-        if (!infile.is_open())
-        {
-            std::cout << "Failed to open the file " << argv[1] << std::endl;
-            std::exit(1);
-        }
-
-        outfile.open((line += argv[1]) += ".replace");
-        line.clear();
-
-        while (!getline(infile, line).eof())
-        {
-            line.find(argv[2]);
-            outfile << line << std::endl;
-            line.clear();
-        }
-        infile.close();
-        outfile.close();
+        std::cout << "Failed to open the file " << argv[1] << std::endl;
+        std::exit(1);
     }
+
+    outfile.open((line += argv[1]) += ".replace");
+    line.clear();
+
+    while (!getline(infile, line).eof())
+    {
+        line.find(argv[2]);
+        outfile << line << std::endl;
+        line.clear();
+    }
+    infile.close();
+    outfile.close();
     return (0);
 }
