2d7a1a55e9e1d69df3388858bf1c9b34de77ff6f
esc-ouni <idouni@student.1337.ma>
2023-10-04 09:44:20 +0100

    prgrs

--- M_01/ex05/main.cpp
+++ M_01/ex05/main.cpp
@@ -1,10 +1,5 @@
 #include "Harl.hpp"
 
-
-void    func(void){
-    std::cout << "Hola Morpho Loco!" << std::endl;
-}
-
 void exit_err(std::string err_msg){
     std::cerr << "==> Err: " + err_msg << std::endl;
     std::exit(1);
@@ -19,13 +14,37 @@ int   contains_only_nums(std::string string){
     }
     return (std::atoi(string.c_str()));
 }
+
 typedef void (*MyPtr)(void);
+
+void    func(void){
+    std::cout << "Hola Morpho Loco!" << std::endl;
+}
+
+void    PP(void){
+    std::cout << "Hola Morpho Laca!" << std::endl;
+}
+
+void    KK(void){
+    std::cout << "Hola Morpho bara!" << std::endl;
+}
 
 int main()
 {
 
-    MyPtr Fptr = &func;
+    MyPtr *Fptr = new MyPtr[4];
 
-    Fptr();
+    Fptr[0] = &func;
+    Fptr[1] = &PP;
+    Fptr[2] = &KK;
+    Fptr[3] = NULL;
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (Fptr[i])
+            Fptr[i]();
+        else
+            break ;
+    }
     return (0);
 }
