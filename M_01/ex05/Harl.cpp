d7a53db6ae950adc7a9ad24d628e9fc0d372ac8c
esc-ouni <idouni@student.1337.ma>
2023-10-04 10:48:19 +0100

    prgrs

--- M_01/ex05/Harl.cpp
+++ M_01/ex05/Harl.cpp
@@ -3,22 +3,6 @@
 Harl::~Harl(void){
 };
 
-void    Harl::debug(void){
-    std::cout << "DEBUG" << std::endl;
-};
-
-void    Harl::info(void){
-    std::cout << "INFO" << std::endl;
-};
-
-void    Harl::warning(void){
-    std::cout << "WARNING" << std::endl;
-};
-
-void    Harl::error(void){
-    std::cout << "ERROR" << std::endl;
-};
-
 void    Harl::complain(std::string level){
     int n = contains_only_nums(level);
 
--- M_01/ex05/Harl.hpp
+++ M_01/ex05/Harl.hpp
@@ -14,29 +14,86 @@
 
 typedef void (*MemberFunc)(void);
 
+// class Harl
+// {
+//     private:
+//         void    debug(void);
+//         void    info(void);
+//         void    warning(void);
+//         void    error(void);
+//         MemberFunc  *MFptr = NULL; 
+//     public:
+//         Harl(void);
+//         ~Harl(void);
+//         void    complain(std::string level);
+// };
+
+// void    Harl::debug(void){
+//     std::cout << "DEBUG" << std::endl;
+// };
+
+// void    Harl::info(void){
+//     std::cout << "INFO" << std::endl;
+// };
+
+// void    Harl::warning(void){
+//     std::cout << "WARNING" << std::endl;
+// };
+
+// void    Harl::error(void){
+//     std::cout << "ERROR" << std::endl;
+// };
+
+// Harl::Harl(void){
+//     this->MFptr = new MemberFunc[5];
+
+//     this->MFptr[0] = &Harl::debug;
+//     this->MFptr[1] = &Harl::info;
+//     this->MFptr[2] = &Harl::warning;
+//     this->MFptr[3] = &Harl::error;
+//     this->MFptr[4] = NULL;
+// };
+
 class Harl
 {
-    private:
-        void    debug(void);
-        void    info(void);
-        void    warning(void);
-        void    error(void);
-        MemberFunc  *MFptr; 
-    public:
-        Harl(void);
-        ~Harl(void);
-        void    complain(std::string level);
+private:
+    typedef void (Harl::*MemberFunc)(void); // Change the type of MemberFunc
+    void debug(void);
+    void info(void);
+    void warning(void);
+    void error(void);
+    MemberFunc *MFptr; // Use nullptr instead of NULL
+public:
+    Harl(void);
+    ~Harl(void);
+    void complain(std::string level);
 };
 
-Harl::Harl(void){
-    this->MFptr = new MemberFunc[5];
+void Harl::debug(void){
+    std::cout << "DEBUG" << std::endl;
+}
 
+void Harl::info(void){
+    std::cout << "INFO" << std::endl;
+}
+
+void Harl::warning(void){
+    std::cout << "WARNING" << std::endl;
+}
+
+void Harl::error(void){
+    std::cout << "ERROR" << std::endl;
+}
+
+Harl::Harl(void)
+{
     this->MFptr[0] = &Harl::debug;
     this->MFptr[1] = &Harl::info;
     this->MFptr[2] = &Harl::warning;
     this->MFptr[3] = &Harl::error;
-    this->MFptr[4] = NULL;
-};
+    this->MFptr[4] = nullptr;
+}
+
 
 void    exit_err(std::string err_msg);
 int     contains_only_nums(std::string string);
