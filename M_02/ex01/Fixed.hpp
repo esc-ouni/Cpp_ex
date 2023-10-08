6475a254624933023f96612d469bff086641ac63
esc-ouni <idouni@student.1337.ma>
2023-10-08 17:28:41 +0100

    Zimmer

--- M_02/ex01/Fixed.hpp
+++ M_02/ex01/Fixed.hpp
@@ -15,9 +15,13 @@ class Fixed
         const static int num_fract_bits = 8;
         int              raw_bits;
     public:
-        Fixed(void);
+        Fixed(const int num_to_convert);
+        Fixed(const float num_to_convert);
+        float toFloat( void ) const;
+        int toInt( void ) const;
         Fixed(Fixed &r_inst);
         Fixed &operator=(Fixed &r_inst);
+        Fixed &operator<<(void);
         ~Fixed(void);
         int getRawBits(void) const;
         void setRawBits(int const raw);
