09be2371faa5d0cc0e94cd90ded998ebd3b8bb49
esc-ouni <idouni@student.1337.ma>
2023-10-08 15:56:37 +0100

    Zimmer

--- M_02/ex00/Fixed.cpp
+++ M_02/ex00/Fixed.cpp
@@ -1,26 +1,31 @@
 #include "Fixed.hpp"
 
 Fixed::Fixed(void){
+    std::cout << "Default constructor called" << std::endl;
 };
 
 Fixed::Fixed(Fixed &r_inst){
-    this->fixed_point = r_inst.fixed_point;
-    this->raw_bits = r_inst.raw_bits;
+    std::cout << "Copy constructor called" << std::endl;
+    *this = r_inst;
 };
 
 Fixed &Fixed::operator=(Fixed &r_inst){
+    std::cout << "Copy assignment operator called" << std::endl;
     this->fixed_point = r_inst.fixed_point;
-    this->raw_bits = r_inst.raw_bits;
+    this->raw_bits = r_inst.getRawBits();
     return (*this);
 };
 
 Fixed::~Fixed(void){
+    std::cout << "Destructor called" << std::endl;
 };
 
 int Fixed::getRawBits(void) const{
+    std::cout << "getRawBits member function called" << std::endl;
     return (this->raw_bits);
 };
 
 void Fixed::setRawBits(int const raw){
+    std::cout << "setRawBits member function called" << std::endl;
     this->raw_bits = raw;
 };
--- M_02/ex00/main.cpp
+++ M_02/ex00/main.cpp
@@ -1,16 +1,15 @@
 #include "Fixed.hpp"
 
 int main( void ) {
-    // int a = 5, b = 18, c = 4, d = 2, r = 0;
-    // Fixed a;
-    // Fixed b( a );
-    // Fixed c;
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
     
-    // c = b;
+    c = b;
 
-    // std::cout << a.getRawBits() << std::endl;
-    // std::cout << b.getRawBits() << std::endl;
-    // std::cout << c.getRawBits() << std::endl;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
 
     return 0;
 }
