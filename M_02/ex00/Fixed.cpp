5d6c95ab9b4ae491c51167cbcaeea2d106d54e38
esc-ouni <idouni@student.1337.ma>
2023-10-08 11:13:06 +0100

    New_clustr

--- M_02/ex00/Fixed.cpp
+++ M_02/ex00/Fixed.cpp
@@ -1,17 +1,21 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed(void)
-{
-}
+Fixed::Fixed(void){
+};
 
-Fixed::Fixed(Fixed &other)
-{
-}
+Fixed::Fixed(Fixed &r_inst){
+};
 
-Fixed Fixed::operator=(Fixed &other)
-{
-}
+Fixed Fixed::operator=(Fixed &r_inst){
+};
 
-Fixed::~Fixed(void)
-{
-}
+Fixed::~Fixed(void){
+};
+
+int Fixed::getRawBits(void) const{
+    return (this->raw_bits);
+};
+
+void Fixed::setRawBits(int const raw){
+    this->raw_bits = raw;
+};
--- M_02/ex00/Fixed.hpp
+++ M_02/ex00/Fixed.hpp
@@ -7,17 +7,19 @@
 #include <cstdlib>
 #include <climits>
 
-
 class Fixed
 {
     private:
-        int              int_s;
-        static const int n_fbits = 8;
+        int              fixed_point;
+        const static int num_fract_bits = 8;
+        int              raw_bits;
     public:
         Fixed(void);
-        Fixed(Fixed &other);
-        Fixed operator=(Fixed &other);
+        Fixed(Fixed &r_inst);
+        Fixed operator=(Fixed &r_inst);
         ~Fixed(void);
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
 };
 
 #endif
