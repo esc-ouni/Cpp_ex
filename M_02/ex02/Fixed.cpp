4e5692a7fe68db98512158920ad4090d1afb34e6
esc-ouni <idouni@student.1337.ma>
2023-10-16 19:23:52 +0100

    Tb_DEBUGED

---
+++ M_02/ex02/Fixed.cpp
@@ -0,0 +1,62 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void){
+    this->fixed_point = 0;
+    std::cout << "Default constructor called" << std::endl;
+};
+
+Fixed::Fixed(Fixed const &r_inst){
+    std::cout << "Copy constructor called" << std::endl;
+    *this = r_inst;
+};
+
+Fixed::Fixed(const int num_to_convert){
+    std::cout << "Int constructor called" << std::endl;
+    if (num_to_convert <= 8388607 && num_to_convert >= -8388608){
+        this->fixed_point = num_to_convert << 8;
+    }
+    else
+        std::cout << "Num to convert out of range" << std::endl;
+};
+
+Fixed::Fixed(const float num_to_convert){
+    std::cout << "Float constructor called" << std::endl;
+    if (num_to_convert <= 8388607 && num_to_convert >= -8388608){
+        this->fixed_point = std::roundf(num_to_convert * 256);
+    }
+    else
+        std::cout << "Num to convert out of range" << std::endl;
+};
+
+Fixed &Fixed::operator=(Fixed const &r_inst){
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->fixed_point = r_inst.getRawBits();
+    return (*this);
+};
+
+std::ostream &operator<<(std::ostream &cout, Fixed const &r_inst){
+    cout << r_inst.toFloat();
+    return (cout);
+};
+
+int Fixed::getRawBits(void) const{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (this->fixed_point);
+};
+
+void Fixed::setRawBits(int const raw){
+    std::cout << "setRawBits member function called" << std::endl;
+    this->fixed_point = raw;
+};
+
+float Fixed::toFloat( void ) const{
+    return((float)this->fixed_point / 256);
+};
+
+int Fixed::toInt( void ) const{
+    return((this->fixed_point >> 8));
+};
+
+Fixed::~Fixed(void){
+    std::cout << "Destructor called" << std::endl;
+};
---
+++ M_02/ex02/Fixed.hpp
@@ -0,0 +1,37 @@
+#ifndef __HEAD_HPP_
+#define __HEAD_HPP_
+
+#define VALUE 42
+
+static int i = 0;
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <climits>
+#include <cmath>
+
+class Fixed
+{
+    private:
+        int              fixed_point;
+        const static int num_fract_bits = 8;
+    public:
+        Fixed(void);
+        Fixed(const int num_to_convert);
+        Fixed(const float num_to_convert);
+        Fixed(Fixed const &r_inst);
+
+        Fixed &operator=(Fixed const &r_inst);
+        int   getRawBits(void) const;
+        void  setRawBits(int const raw);
+        float toFloat( void ) const;
+        int   toInt( void ) const;
+
+        ~Fixed(void);
+};
+
+std::ostream &operator<<(std::ostream &cout, Fixed const &r_inst);
+
+#endif
