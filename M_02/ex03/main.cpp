3723f451158a7db695fc59fbae684b2e70fb8ea3
esc-ouni <idouni@student.1337.ma>
2023-10-19 11:52:45 +0100

    TBAOC

--- M_02/ex03/main.cpp
+++ M_02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 11:17:18 by idouni            #+#    #+#             */
-/*   Updated: 2023/10/19 11:48:25 by idouni           ###   ########.fr       */
+/*   Updated: 2023/10/19 11:52:35 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,16 @@ int main( void ) {
     Point point5(1, 3);
     Point point6(2, 0);
     Point point7(4, 2);
+    Point point8(4, 4);
 
-    std::cout << "Point 1 is inside the triangle: " << std::boolalpha << bsp(a, b, c, point1) << std::endl;
-    std::cout << "Point 2 is inside the triangle: " << std::boolalpha << bsp(a, b, c, point2) << std::endl;
-    std::cout << "Point 3 is inside the triangle: " << std::boolalpha << bsp(a, b, c, point3) << std::endl;
-    std::cout << "Point 4 is inside the triangle: " << std::boolalpha << bsp(a, b, c, point4) << std::endl;
-    std::cout << "Point 5 is inside the triangle: " << std::boolalpha << bsp(a, b, c, point5) << std::endl;
-    std::cout << "Point 6 is inside the triangle: " << std::boolalpha << bsp(a, b, c, point6) << std::endl;
-    std::cout << "Point 7 is inside the triangle: " << std::boolalpha << bsp(a, b, c, point7) << std::endl;
+    std::cout << "Point 1 in : " << std::boolalpha << bsp(a, b, c, point1) << std::endl;
+    std::cout << "Point 2 in : " << std::boolalpha << bsp(a, b, c, point2) << std::endl;
+    std::cout << "Point 3 in : " << std::boolalpha << bsp(a, b, c, point3) << std::endl;
+    std::cout << "Point 4 in : " << std::boolalpha << bsp(a, b, c, point4) << std::endl;
+    std::cout << "Point 5 in : " << std::boolalpha << bsp(a, b, c, point5) << std::endl;
+    std::cout << "Point 6 in : " << std::boolalpha << bsp(a, b, c, point6) << std::endl;
+    std::cout << "Point 7 in : " << std::boolalpha << bsp(a, b, c, point7) << std::endl;
+    std::cout << "Point 8 in : " << std::boolalpha << bsp(a, b, c, point8) << std::endl;
 
     // std::cout << bsp(a, b, c, point1) << std::endl;
     // std::cout << bsp(a, b, c, point2) << std::endl;
