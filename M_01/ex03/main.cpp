a6f170f9c0d62ab6f4136ab4411e72d80525ec84
esc-ouni <idouni@student.1337.ma>
2023-10-19 16:24:32 +0100

    TBAOC

--- M_01/ex03/main.cpp
+++ M_01/ex03/main.cpp
@@ -1,15 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/19 16:17:40 by idouni            #+#    #+#             */
+/*   Updated: 2023/10/19 16:24:06 by idouni           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Weapon.hpp"
 #include "HumanA.hpp"
 #include "HumanB.hpp"
 
+// void leak()
+// {
+//     std::system("leaks Uviolence");
+// }
+
 int main()
 {
-
-    Weapon club = Weapon("crude spiked club");
-    HumanA bob("Bob", club);
-    bob.attack();
-    club.setType("some other type of club");
-    bob.attack();
+    // std::atexit(leak);
+    {
+        Weapon club = Weapon("crude spiked club");
+        HumanA bob("Bob", club);
+        bob.attack();
+        club.setType("some other type of club");
+        bob.attack();
+    }
     {
         Weapon club = Weapon("crude spiked club");
         HumanB jim("Jim");
@@ -18,4 +37,5 @@ int main()
         club.setType("some other type of club");
         jim.attack();
     }
+    return 0;
 }
