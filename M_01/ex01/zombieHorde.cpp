fa1db1617cc0976288850e50076fdc1ea6a28297
esc-ouni <idouni@student.1337.ma>
2023-10-19 16:06:24 +0100

    TBAOC

--- M_01/ex01/zombieHorde.cpp
+++ M_01/ex01/zombieHorde.cpp
@@ -3,7 +3,7 @@
 Zombie* zombieHorde(int N, std::string name)
 {
     Zombie *Zombies = NULL;
-    if (N > 0 && N <= INT_MAX){
+    if (N > 0){
         Zombies = new Zombie[N];
         if (Zombies){
             for (int i = 0; i < N; i++){
