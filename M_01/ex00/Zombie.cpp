778c1a75afc1b88acd494c00f1477d0d4e56384b
esc-ouni <idouni@student.1337.ma>
2023-10-19 14:57:26 +0100

    TBAOC

--- M_01/ex00/Zombie.cpp
+++ M_01/ex00/Zombie.cpp
@@ -6,30 +6,18 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 13:43:34 by idouni            #+#    #+#             */
-/*   Updated: 2023/10/19 14:28:21 by idouni           ###   ########.fr       */
+/*   Updated: 2023/10/19 14:54:14 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie(std::string name){
-    // if (name.c_str() != NULL)
-    //     this->name = name;
-    // else
-    (void)name;
-        this->name = "hello";
-}; 
+Zombie::Zombie(std::string name):name(name){}; 
 
 Zombie::~Zombie(){
-    // if (!this->name.empty())
-    //     std::cout << "~" << this->name << "\tDestroyed" << std::endl;
-    // else
-        std::cout << "~Instance\tDestroyed" << std::endl;
+    std::cout << "~" << this->name << "\tDestroyed" << std::endl;
 };
 
 void    Zombie::announce( void ){
-    // if (!this->name.empty())
-    //     std::cout << this->name << "\t: BraiiiiiiinnnzzzZ..." << std::endl;
-    // else
-        std::cout << "\t: BraiiiiiiinnnzzzZ..." << std::endl;
+    std::cout << this->name << "\t: BraiiiiiiinnnzzzZ..." << std::endl;
 }
--- M_01/ex00/main.cpp
+++ M_01/ex00/main.cpp
@@ -6,42 +6,27 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 13:43:14 by idouni            #+#    #+#             */
-/*   Updated: 2023/10/19 14:51:29 by idouni           ###   ########.fr       */
+/*   Updated: 2023/10/19 14:57:06 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-// int main()
-// {
-//     Zombie *a_Zombie;
-//     Zombie *b_Zombie;
-//     Zombie Zombie("HOLLS");
+int main()
+{
+    Zombie *a_Zombie;
+    Zombie *b_Zombie;
+    Zombie Zombiea("HOLLS");
+    Zombie Zombiee("KOLLS");
 
-//     Zombie.announce();
-//     a_Zombie = newZombie("NULL");
-//     a_Zombie->announce();
-//     b_Zombie = newZombie(NULL);
-//     a_Zombie->announce();
-//     delete a_Zombie;
-// }
+    a_Zombie = newZombie("NULL");
+    b_Zombie = newZombie("BOLLS");
 
-int main() {
-    std::string str = NULL;
+    Zombiea.announce();
+    Zombiee.announce();
+    a_Zombie->announce();
+    b_Zombie->announce();
 
-    // if (str.c_str() != NULL)
-    //     std::cout << str.c_str() << std::endl;
-
-    // std::cout <<  << std::endl;
-
-    
-    // std::cout << str.~basic_string() << std::endl;
-    
-    // if (str.~basic_string()) {
-    //     std::cout << "The string is empty." << std::endl;
-    // } else {
-    //     std::cout << "The string is not empty." << std::endl;
-    // }
-
-    return 0;
+    delete a_Zombie;
+    delete b_Zombie;
 }
