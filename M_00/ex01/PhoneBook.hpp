2181e25589959f8d51e0806f638e344eab4a4f6c
esc-ouni <idouni@student.1337.ma>
2023-10-22 11:54:03 +0100

    LESGO

--- M_00/ex01/PhoneBook.hpp
+++ M_00/ex01/PhoneBook.hpp
@@ -6,9 +6,11 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 17:56:29 by idouni            #+#    #+#             */
-/*   Updated: 2023/10/22 11:28:35 by idouni           ###   ########.fr       */
+/*   Updated: 2023/10/22 11:52:01 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
 
 #ifndef __PHONE_BOOK_
 # define __PHONE_BOOK_
@@ -17,7 +19,6 @@
 # include <iostream>
 # include <string>
 # include "Contact.hpp"
-#include "PhoneBook.hpp"
 
 static int indexx = 0;
 
