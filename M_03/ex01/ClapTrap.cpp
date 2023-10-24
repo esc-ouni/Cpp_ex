76ca715e6e35fa70382878d0aed2745785e08ef3
esc-ouni <idouni@student.1337.ma>
2023-10-24 15:00:31 +0100

    1st_Cpp

---
+++ M_03/ex01/ClapTrap.cpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/22 19:06:16 by idouni            #+#    #+#             */
+/*   Updated: 2023/10/24 14:40:41 by idouni           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string Name){
+    this->Name = Name;
+    this->Hit_Points = 10;
+    this->Energy_Points = 10;
+    this->Attack_Damage = 0;
+    std::cout << "ClapTrap " << this->Name << " constructor called" << std::endl;
+};
+
+ClapTrap::ClapTrap(ClapTrap const &r_inst){
+    *this = r_inst;
+};
+
+ClapTrap::~ClapTrap(){
+    std::cout << "ClapTrap " << this->Name << " destructor called" << std::endl;
+};
+
+ClapTrap &ClapTrap::operator=(ClapTrap const &r_inst){
+    this->Name = r_inst.Name;
+    this->Hit_Points = r_inst.Hit_Points;
+    this->Energy_Points = r_inst.Energy_Points;
+    this->Attack_Damage = r_inst.Attack_Damage;
+    return (*this);
+};
+
+void ClapTrap::attack(std::string const &target){
+    std::cout << "ClapTrap " << this->Name << " attacks " << target << ", causing " << this->Attack_Damage << " points of damage!" << std::endl;
+};
+
+void ClapTrap::takeDamage(unsigned int amount){
+    if (amount >= this->Hit_Points)
+        this->Hit_Points = 0;
+    else
+        this->Hit_Points -= amount;
+    std::cout << "ClapTrap " << this->Name << " takes " << amount << " points of damage!" << std::endl;
+};
+
+void ClapTrap::beRepaired(unsigned int amount){
+    if (this->Energy_Points > 0 && this->Hit_Points)
+        this->Energy_Points -= 1;
+    else {
+        std::cout << "ClapTrap " << this->Name << " has nothing to do!" << std::endl;
+        return ;   
+    }
+    this->Hit_Points += amount;
+};
---
+++ M_03/ex01/ClapTrap.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/22 19:06:24 by idouni            #+#    #+#             */
+/*   Updated: 2023/10/24 14:15:37 by idouni           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __CLAP_TRAP_
+# define __CLAP_TRAP_
+
+# include <iostream>
+# include <string>
+
+class ClapTrap{
+        std::string     Name;
+        unsigned int    Hit_Points;
+        unsigned int    Energy_Points;
+        unsigned int    Attack_Damage;
+    public:
+        ClapTrap();
+        ClapTrap(std::string name);
+        ClapTrap(ClapTrap const &obj);
+        ~ClapTrap();
+        ClapTrap &operator=(ClapTrap const &obj);
+        
+        void attack(std::string const &target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+};
+
+#endif
