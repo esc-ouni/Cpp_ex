caa33e58d5b2200d2db6e71d68ea4294e6dad413
esc-ouni <idouni@student.1337.ma>
2023-10-24 17:23:07 +0100

    Clap_trap

--- M_03/ex00/main.cpp
+++ M_03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 18:46:46 by idouni            #+#    #+#             */
-/*   Updated: 2023/10/24 14:36:28 by idouni           ###   ########.fr       */
+/*   Updated: 2023/10/24 17:22:25 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main()
 {
     ClapTrap clap("clap");
-    ClapTrap clap2(clap);
+    ClapTrap clap2("clap2");
     clap2.attack("clap");
     clap2.takeDamage(50000);
     clap2.beRepaired(5);
