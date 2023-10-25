2a1ecf6bedc6a741712be2a18a6c4dbe508f6fc3
esc-ouni <idouni@student.1337.ma>
2023-10-25 20:25:08 +0100

    Clap_trap

--- M_03/ex01/ScavTrap.cpp
+++ M_03/ex01/ScavTrap.cpp
@@ -6,12 +6,12 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 14:43:18 by idouni            #+#    #+#             */
-/*   Updated: 2023/10/25 19:36:24 by idouni           ###   ########.fr       */
+/*   Updated: 2023/10/25 20:24:38 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 
 void ScavTrap::guardGate(){
-    std::cout << "ScavTrap" << Scavtrap  << this->Name << "is now in Gate keeper mode" << std::endl;
+    std::cout << "ScavTrap " << this->Name << "\tis now in Gate keeper mode" << std::endl;
 };
