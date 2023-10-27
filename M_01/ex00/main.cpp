ffdc67d254a703033809d6d5b47de0f653a24109
esc-ouni <idouni@student.1337.ma>
2023-10-27 17:09:10 +0100

    Diamond_Trap

--- M_01/ex00/main.cpp
+++ M_01/ex00/main.cpp
@@ -6,18 +6,21 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 13:43:14 by idouni            #+#    #+#             */
-/*   Updated: 2023/10/19 14:57:06 by idouni           ###   ########.fr       */
+/*   Updated: 2023/10/27 17:08:53 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
+void leak(){std::system("leaks Zombie | grep \" leaks \" | awk \'{print \" ==> \" $4\" \"$3}\'");}
+
 int main()
 {
+    atexit(leak);
     Zombie *a_Zombie;
     Zombie *b_Zombie;
-    Zombie Zombiea("HOLLS");
     Zombie Zombiee("KOLLS");
+    Zombie Zombiea("HOLLS");
 
     a_Zombie = newZombie("NULL");
     b_Zombie = newZombie("BOLLS");
