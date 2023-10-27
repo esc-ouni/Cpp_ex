1a4e0638e89a2256307212530275f2bb0ef11c2e
esc-ouni <idouni@student.1337.ma>
2023-10-27 16:00:57 +0100

    Diamond_Trap

--- M_03/ex03/DiamondTrap.cpp
+++ M_03/ex03/DiamondTrap.cpp
@@ -6,19 +6,23 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 21:23:49 by idouni            #+#    #+#             */
-/*   Updated: 2023/10/27 15:59:14 by idouni           ###   ########.fr       */
+/*   Updated: 2023/10/27 16:00:32 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
 
-DiamondTrap::DiamondTrap(std::string name){};
+DiamondTrap::DiamondTrap(std::string name){
+};
 
-DiamondTrap::DiamondTrap(DiamondTrap const &r_inst){};
+DiamondTrap::DiamondTrap(DiamondTrap const &r_inst){
+};
 
-DiamondTrap::~DiamondTrap(){};
+DiamondTrap::~DiamondTrap(){
+};
 
-DiamondTrap &DiamondTrap::operator=(DiamondTrap const &r_inst){};
+DiamondTrap &DiamondTrap::operator=(DiamondTrap const &r_inst){
+};
 
 DiamondTrap::whoAmI(){
     std::cout << "My name is " << this->_name << std::endl;
