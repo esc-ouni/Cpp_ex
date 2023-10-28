96bc783838b5cf75a7abcedd2340f1b0eb2d6131
esc-ouni <idouni@student.1337.ma>
2023-10-28 18:49:21 +0100

    Diamond_Trap

--- M_01/ex01/zombieHorde.cpp
+++ M_01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 16:08:32 by idouni            #+#    #+#             */
-/*   Updated: 2023/10/19 16:08:33 by idouni           ###   ########.fr       */
+/*   Updated: 2023/10/28 18:46:41 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ Zombie* zombieHorde(int N, std::string name)
 {
     Zombie *Zombies = NULL;
     if (N > 0){
-        Zombies = new Zombie[N];
+        Zombies = new(std::nothrow) Zombie[N];
         if (Zombies){
             for (int i = 0; i < N; i++){
                 Zombies[i].setName(name);
