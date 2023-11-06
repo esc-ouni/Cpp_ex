1416e77a4c1887c6b420d029629975e47488060d
esc-ouni <idouni@student.1337.ma>
2023-11-06 14:33:16 +0100

    MMM

--- M_03/ex01/ScavTrap.cpp
+++ M_03/ex01/ScavTrap.cpp
@@ -6,18 +6,26 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 14:43:18 by idouni            #+#    #+#             */
-/*   Updated: 2023/10/25 21:05:10 by idouni           ###   ########.fr       */
+/*   Updated: 2023/11/06 14:32:08 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap(): ClapTrap(){
+    this->Name = "Without";
+    this->Hit_Points = 100;
+    this->Energy_Points = 50;
+    this->Attack_Damage = 20;
+    std::cout << "ScavTrap " << this->Name << "\tDefault constructor called" << std::endl;
+};
 
 ScavTrap::ScavTrap(std::string Name): ClapTrap(Name){
     this->Name = Name;
     this->Hit_Points = 100;
     this->Energy_Points = 50;
     this->Attack_Damage = 20;
-    std::cout << "ScavTrap " << this->Name << "\tconstructor called (in ScavTrap way)" << std::endl;
+    std::cout << "ScavTrap " << this->Name << "\tConstructor called" << std::endl;
 };
 
 ScavTrap::ScavTrap(ScavTrap const &r_inst): ClapTrap(r_inst){
@@ -25,7 +33,7 @@ ScavTrap::ScavTrap(ScavTrap const &r_inst): ClapTrap(r_inst){
 };
 
 ScavTrap::~ScavTrap(){
-    std::cout << "ScavTrap " << this->Name << "\tdestructor called (in ScavTrap way)" << std::endl;
+    std::cout << "ScavTrap " << this->Name << "\tdestructor called" << std::endl;
 };
 
 ScavTrap &ScavTrap::operator=(ScavTrap const &r_inst){
@@ -40,10 +48,10 @@ void ScavTrap::attack(std::string const &target){
     if (this->Energy_Points > 0 && this->Hit_Points)
         this->Energy_Points -= 1;
     else {
-        std::cout << "ScavTrap " << this->Name << "\tdoesn't have requirement to get attack (in ScavTrap way)!" << std::endl;
+        std::cout << "ScavTrap " << this->Name << "\tdoesn't have requirement to get attack!" << std::endl;
         return ;   
     }
-    std::cout << "ScavTrap " << this->Name << "\tattacks " << target << ", causing " << this->Attack_Damage << " points of damage (in ScavTrap way)!" << std::endl;
+    std::cout << "ScavTrap " << this->Name << "\tattacks " << target << ", causing " << this->Attack_Damage << " points of damage!" << std::endl;
 };
 
 void ScavTrap::guardGate(){
--- M_03/ex01/main.cpp
+++ M_03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 18:46:46 by idouni            #+#    #+#             */
-/*   Updated: 2023/10/25 19:35:04 by idouni           ###   ########.fr       */
+/*   Updated: 2023/11/06 14:30:13 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,11 @@ int main()
     scav1.attack("clap");
     scav1.attack("clap");
     scav1.attack("clap");
-    clap1.takeDamage(100);
+    clap1.takeDamage(1000000);
     clap1.attack("scav");
     clap1.beRepaired(5);
     scav1.beRepaired(10);
     scav1.guardGate();
-    
+
     return 0;
 }
