97e762000cc7d510cfd8a5cbf9f723433fc9c4c1
esc-ouni <idouni@student.1337.ma>
2023-11-08 14:58:18 +0100

    Brain

---
+++ M_04/ex01/Brain.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Brain.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 14:05:49 by idouni            #+#    #+#             */
+/*   Updated: 2023/11/08 14:48:21 by idouni           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __BRAIN_
+#define __BRAIN_
+
+#include <iostream>
+
+class Brain{
+    private:
+        std::string ideas[100];
+    public:
+        Brain();
+        Brain(Brain const &r_inst);
+        Brain &operator=(Brain const &r_inst);
+        ~Brain();
+
+        std::string getIdea(int index) const;
+        void setIdea(int index, std::string idea);
+};
+
+#endif
--- M_04/ex01/main.cpp
+++ M_04/ex01/main.cpp
@@ -6,34 +6,43 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 12:17:05 by idouni            #+#    #+#             */
-/*   Updated: 2023/11/08 14:03:31 by idouni           ###   ########.fr       */
+/*   Updated: 2023/11/08 14:58:05 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "Brain.hpp"
 
-// void leak(){std::cout<<std::endl;std::system("leaks Polymorph | grep \" leaks \" | awk '{ print $3\" \"$4 }'");std::cout << std::endl;}
+// void leak(){std::cout<<std::endl;std::system("leaks Brain | grep \" leaks \" | awk '{ print $3\" \"$4 }'");std::cout << std::endl;}
 
 int main(){
     // std::atexit(leak);
-    const Animal* meta = new Animal();
-    const Animal* dog = new Dog();
-    const Animal* cat = new Cat();
+    // const Animal* meta = new Animal();
+    // const Animal* dog = new Dog();
+    // const Animal* cat = new Cat();
     
-    std::cout << std::endl;
-    std::cout << dog->getType() << " " << std::endl;
-    std::cout << cat->getType() << " " << std::endl 
-              << std::endl;
+    // std::cout << std::endl;
+    // std::cout << dog->getType() << " " << std::endl;
+    // std::cout << cat->getType() << " " << std::endl 
+    //           << std::endl;
               
-    cat->makeSound();
-    dog->makeSound();
-    meta->makeSound();
-    std::cout << std::endl;
+    // cat->makeSound();
+    // dog->makeSound();
+    // meta->makeSound();
+    // std::cout << std::endl;
             
-    delete meta;
-    delete cat;
-    delete dog;
+    // delete meta;
+    // delete cat;
+    // delete dog;
+    
+    Brain brain;
+
+    for (int i = 1; i <= 100; i++){
+        std::cout << brain.getIdea(i) << std::endl;
+    }
+    std::cout <<  "Bye !!" << std::endl;
+
     return 0;
 }
