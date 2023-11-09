4827079219d3dcc27ea6f34c0864da3be36a364e
esc-ouni <idouni@student.1337.ma>
2023-11-09 15:22:40 +0100

    MMM

--- M_04/ex01/main.cpp
+++ M_04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 12:17:05 by idouni            #+#    #+#             */
-/*   Updated: 2023/11/09 14:45:28 by idouni           ###   ########.fr       */
+/*   Updated: 2023/11/09 15:22:20 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,27 @@
 #include "Cat.hpp"
 #include "Brain.hpp"
 
-void leak(){std::cout<<std::endl;std::system("leaks Brain | grep \" leaks \" | awk '{ print $3\" \"$4 }'");std::cout << std::endl;}
+// void leak(){std::cout<<std::endl;std::system("leaks Brain | grep \" leaks \" | awk '{ print $3\" \"$4 }'");std::cout << std::endl;}
 
 int main(){
-    std::atexit(leak);    
-    // const Animal* dog = new Dog();
-    // const Animal* cat = new Cat();
+    // std::atexit(leak);
+    int N = 20;
+    Animal array_of_animals[N];
     
-    // delete dog;
-    // delete cat;
+    for (int i = 0; i < N; i++){
+        if (i < N/2)
+            array_of_animals[i].
+    };
+
 
 
     // Dog *dog1 = new Dog();
     // Dog *dog2 = new Dog();
 
+    // *(dog2) = *(dog1);
 
     // delete dog1;
     // delete dog2;
-    // delete dog1;
-
-    // for (int i = 1; i <= 100; i++){
-    //     std::cout << dog.getIdea(i) << std::endl;
-    // }
-    // std::cout <<  "Bye !!" << std::endl;
-
+    
     return 0;
 }
