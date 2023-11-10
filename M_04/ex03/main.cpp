086a74e482b5dcd9f91b7b7c225d6f76926e208a
esc-ouni <idouni@student.1337.ma>
2023-11-10 11:24:20 +0100

    TYPING

--- M_04/ex03/main.cpp
+++ M_04/ex03/main.cpp
@@ -6,17 +6,38 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 12:17:05 by idouni            #+#    #+#             */
-/*   Updated: 2023/11/09 18:56:28 by idouni           ###   ########.fr       */
+/*   Updated: 2023/11/10 11:23:50 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AMateria.hpp"
+#include "Character.hpp"
+#include "Cure.hpp"
 #include "ICharacter.hpp"
+#include "IMateriaSource.hpp"
+#include "Ice.hpp"
+#include "MateriaSource.hpp"
 
 // void leak(){std::cout<<std::endl;std::system("leaks Recap | grep \" leaks \" | awk '{ print $3\" \"$4 }'");std::cout << std::endl;}
  // std::atexit(leak);
 
 int main(){
+    
+    IMateriaSource* src = new MateriaSource();
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
+    ICharacter* me = new Character("me");
+    AMateria* tmp;
+    tmp = src->createMateria("ice");
+    me->equip(tmp);
+    tmp = src->createMateria("cure");
+    me->equip(tmp);
+    ICharacter* bob = new Character("bob");
+    me->use(0, *bob);
+    me->use(1, *bob);
+    delete bob;
+    delete me;
+    delete src
     
     return 0;
 }
