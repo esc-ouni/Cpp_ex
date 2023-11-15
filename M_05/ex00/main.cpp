aefd49f2abdae34c17d80fe9b53cee33925c4abb
esc-ouni <idouni@student.1337.ma>
2023-11-15 10:52:53 +0100

    Exceptions

--- M_05/ex00/main.cpp
+++ M_05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 10:24:54 by idouni            #+#    #+#             */
-/*   Updated: 2023/11/15 10:50:46 by idouni           ###   ########.fr       */
+/*   Updated: 2023/11/15 10:52:42 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main(){
     
     Bureaucrat employee("oustad", 1);
-    for (unsigned long long i = 0; i < 844674407370551; i++){
+    for (unsigned long long i = 0; i < 200; i++){
         try{
             employee.decrementGrade();
             std::cout << "Grade decremented successfully !" << employee.getGrade() << std::endl;
