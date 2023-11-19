43c90eba7e4831294625682e700691c0635f5ccf
esc-ouni <idouni@student.1337.ma>
2023-11-19 12:04:59 +0100

    m5

---
+++ M_05/ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/19 12:03:26 by idouni            #+#    #+#             */
+/*   Updated: 2023/11/19 12:03:50 by idouni           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __PRESIDENTIALPARDON_HPP__
+#define __PRESIDENTIALPARDON_HPP__
+
+class PresidentialPardonForm{
+    private:
+
+};
+
+
+#endif
---
+++ M_05/ex02/RobotomyRequestForm.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.hpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/19 12:00:43 by idouni            #+#    #+#             */
+/*   Updated: 2023/11/19 12:01:52 by idouni           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __ROBOTMYREQUEST_HPP_
+#define __ROBOTMYREQUEST_HPP_
+
+class RobotomyRequestForm
+{
+    private:
+
+};
+
+
+#endif
