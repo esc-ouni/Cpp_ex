00696c6f375ef78881f0b6245587a0f60fdc2a04
esc-ouni <idouni@student.1337.ma>
2023-12-21 14:41:54 +0100

    /exepsio

--- M_05/ex02/PresidentialPardonForm.cpp
+++ M_05/ex02/PresidentialPardonForm.cpp
@@ -5,17 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/19 12:02:21 by idouni            #+#    #+#             */
-/*   Updated: 2023/12/20 18:53:04 by idouni           ###   ########.fr       */
+/*   Created: 2023/12/21 14:41:26 by idouni            #+#    #+#             */
+/*   Updated: 2023/12/21 14:41:29 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PresidentialPardonForm.hpp"
 
 PresidentialPardonForm::PresidentialPardonForm():AForm("Wout", 25, 5){
+    std::cout << this->getName() << " has been pardoned by Zaphod Beeblebrox" << std::endl;
 };
 
 PresidentialPardonForm::PresidentialPardonForm(std::string &target):AForm(target, 25, 5){
+    std::cout << this->getName() << " has been pardoned by Zaphod Beeblebrox" << std::endl;
 };
 
 PresidentialPardonForm::PresidentialPardonForm(PresidentialPardonForm &r_inst){
--- M_05/ex02/RobotomyRequestForm.cpp
+++ M_05/ex02/RobotomyRequestForm.cpp
@@ -6,16 +6,20 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 12:00:38 by idouni            #+#    #+#             */
-/*   Updated: 2023/12/20 18:53:19 by idouni           ###   ########.fr       */
+/*   Updated: 2023/12/21 13:32:03 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RobotomyRequestForm.hpp"
 
 RobotomyRequestForm::RobotomyRequestForm():AForm("Wout", 72, 45){
+    std::cout << "Makes some drilling noises" << std::endl;
+    std::cout << this->getName() << " has been robotomized successfully 50\% of the time" << std::endl;
 };
 
 RobotomyRequestForm::RobotomyRequestForm(std::string &target):AForm(target, 72, 45){
+    std::cout << "Makes some drilling noises" << std::endl;
+    std::cout << this->getName() << " has been robotomized successfully 50\% of the time" << std::endl;
 };
 
 RobotomyRequestForm::RobotomyRequestForm(RobotomyRequestForm const &r_inst){
