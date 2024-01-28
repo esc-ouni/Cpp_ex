711227f470956034aaddadd0b8ae31fb155826d6
esc-ouni <idouni@student.1337.ma>
2024-01-28 20:40:07 +0100

    M_07/ex00

--- M_07/ex00/whatever.hpp
+++ M_07/ex00/whatever.hpp
@@ -3,5 +3,22 @@
 
 #include <iostream>
 
+template <typename T>
+void swap(T &arg1, T &arg2){
+    T Temp;
+    Temp = arg1;
+    arg1 = arg2;
+    arg1 = Temp;
+};
+
+template <typename Y>
+Y &min(Y &arg1, Y &arg2){
+    return ((arg1 < arg2) ? arg1 : arg2);
+};
+
+template <typename Z>
+Z &max(Z &arg1, Z &arg2){
+    return ((arg1 > arg2) ? arg1 : arg2);
+};
 
 #endif
