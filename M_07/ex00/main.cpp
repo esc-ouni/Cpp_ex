35cfb205a7b25afd31c9da9df9acd3c7b330576d
esc-ouni <idouni@student.1337.ma>
2024-01-28 20:44:25 +0100

    M_07/ex00

--- M_07/ex00/main.cpp
+++ M_07/ex00/main.cpp
@@ -1,11 +1,9 @@
 #include "whatever.hpp"
 
-int main(){
+int main( void ) {
     int a = 2;
     int b = 3;
-
-    std::cout << "Templates !" << std::endl;
-
+    
     ::swap( a, b );
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
@@ -16,7 +14,6 @@ int main(){
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
     std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-
     return 0;
 }
 
