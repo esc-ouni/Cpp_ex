e280e19da6d988d1283c92b6f9f820470371866c
esc-ouni <idouni@student.1337.ma>
2024-01-30 22:05:20 +0100

    M_08/ex02

--- M_08/ex02/MutantStack.hpp
+++ M_08/ex02/MutantStack.hpp
@@ -2,6 +2,14 @@
 #define __MUTANT_STACK_HPP_
 
 #include <iostream>
+#include <stack>
+
+// class MutantStack : public std::stack {
+//     private:
+//     public:
+//         // iterators
+
+// };
 
 
 
