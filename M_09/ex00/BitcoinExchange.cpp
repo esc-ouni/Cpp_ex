dd54f7f09475aa2668aa5307f89f0ba892bed1b2
esc-ouni <idouni@student.1337.ma>
2024-02-03 14:55:48 +0100

    (M_09/ex00)_init

--- M_09/ex00/BitcoinExchange.cpp
+++ M_09/ex00/BitcoinExchange.cpp
@@ -1,26 +1,29 @@
 #include "BitcoinExchange.hpp"
 
-void exctract_kv(std::string &line, std::map<std::string, unsigned int> &Map){
-    std::stringstream ss;
+void exctract_kv(std::string &line, std::map<std::string, unsigned int> &Map, char dilimeter){
+    std::stringstream stream(line);
 
-    // std::string token;
-    // std::getline(line, token){
-// 
-    // }
+    std::string token, token2;
+    std::getline(stream, token, dilimeter);
+    std::getline(stream, token2, dilimeter);
+    try{
+        Map[token] = std::stoul(token2); }
+    catch(const std::exception& e){
+        Map[token] = 0; }
 };
 
 void __init(int argc, char *argv[], std::map<std::string, unsigned int> &Map){
     std::string line;
 
-    if (argc != 2){
-        std::ifstream infile(argv[1]);
-        if (!infile.is_open())
-            throw std::runtime_error("Error: could not open file.");
+    if (argc != 2)
+        throw std::runtime_error("Error: wrong arg count.");
 
-        while (std::getline(infile, line)){
-            exctract_kv(line, Map);
-        }
+    std::ifstream infile(argv[1]);
+    if (!infile.is_open())
+        throw std::runtime_error("Error: could not open file.");
+
+    while (std::getline(infile, line)){
+        exctract_kv(line, Map, ',');
     }
-
 };
 
--- M_09/ex00/main.cpp
+++ M_09/ex00/main.cpp
@@ -1,16 +1,21 @@
 #include "BitcoinExchange.hpp"
 
+void print(std::map<std::string, unsigned int> &Map){
+    for (auto elem : Map){
+        std::cout << "<"<< elem.first << ">\t= <" << elem.second << ">" << std::endl;
+    }
+}
+
 int main(int argc, char *argv[]){
 
     std::map<std::string, unsigned int> Map;
 
     try{
-        __init(argc, argv, Map);
-    }
+        __init(argc, argv, Map); }
     catch(const std::exception& e){
-        std::cerr << e.what() << std::endl;
-    }
-    
+        std::cerr << e.what() << std::endl; }
+
+    print(Map);
 
     return 0;
 }
