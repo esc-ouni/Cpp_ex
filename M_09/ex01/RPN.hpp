09899f90021dcd9d2560c167a4da8ddc24c655f0
esc-ouni <idouni@student.1337.ma>
2024-02-04 15:14:40 +0100

    M_09/ex01_init

---
+++ M_09/ex01/RPN.hpp
@@ -0,0 +1,14 @@
+#ifndef __RPN_HPP_
+#define __RPN_HPP_
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <map>
+#include <algorithm>
+#include <sstream>
+
+void print(std::map<std::string, double> &Map);
+void __init(int argc, char *argv[]);
+
+#endif
---
+++ M_09/ex01/main.cpp
@@ -0,0 +1,9 @@
+#include "RPN.hpp"
+
+int main(int argc, char *argv[]){
+    try{
+        __init(argc, argv); }
+    catch(const std::exception& e){
+        std::cerr << e.what() << std::endl; }
+    return 0;
+}
