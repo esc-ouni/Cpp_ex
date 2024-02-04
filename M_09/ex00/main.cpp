9f21a94df7eb2e345cadcea14770bba6859cb486
esc-ouni <idouni@student.1337.ma>
2024-02-04 13:24:14 +0100

    M_09/ex00

--- M_09/ex00/main.cpp
+++ M_09/ex00/main.cpp
@@ -1,11 +1,9 @@
 #include "BitcoinExchange.hpp"
 
 int main(int argc, char *argv[]){
-
     try{
         __init(argc, argv); }
     catch(const std::exception& e){
         std::cerr << e.what() << std::endl; }
-
     return 0;
 }
